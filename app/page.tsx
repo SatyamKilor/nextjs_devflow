ca45c9764d93c447d332c5c7817c496df6372537
Satyam Kilor <satyamkilor8.com>
2024-12-10 15:34:52 +0530

    Tailwind CSS setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1 className="text-3xl font-black text-white">Welcome</h1>;
+  return <h1 className="text-3xl font-black text-light-500">Welcome</h1>;
 };
 
 export default Home;
