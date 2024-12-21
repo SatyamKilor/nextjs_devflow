b7aa8073e2c61f0ae9228bdd9273e06c645d3d11
Satyam Kilor <satyamkilor8.com>
2024-12-21 23:42:29 +0530

    Implement Create and Read User Routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,8 +5,6 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import handleError from "@/lib/handlers/error";
-import { ValidationError } from "@/lib/http-errors";
 
 const questions = [
   {
@@ -66,24 +64,10 @@ const questions = [
   },
 ];
 
-const test = async () => {
-  try {
-    throw new ValidationError({
-      title: ["Required"],
-      tags: ['"Javascript" is not a valid tag'],
-    });
-  } catch (error) {
-    return handleError(error);
-  }
-};
-
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 const Home = async ({ searchParams }: SearchParams) => {
-  const result = await test();
-  console.log(result);
-
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
